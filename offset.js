e99ba597adf28f5afb88395e788a52f31102c6b9
williamqian <694619951@qq.com>
2018-03-01 11:08:35 +0800

    init

--- offset.js
+++ offset.js
@@ -4,12 +4,14 @@
  * @param {HTMLElement} ele 
  * @returns { {left: number, top: number} }
  */
-function offset(ele) {
+function offset(ele)
+{
     var pos = {
         left: 0,
         top: 0
     };
-    while (ele) {
+    while (ele)
+    {
         pos.left += ele.offsetLeft;
         pos.top += ele.offsetTop;
         ele = ele.offsetParent;
