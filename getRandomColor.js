a70205a702542ffb580dcb234b8be52c7dba36a1
williamqian <694619951@qq.com>
2018-03-19 10:11:56 +0800

    增加新api

--- getRandomColor.js
+++ getRandomColor.js
@@ -3,8 +3,8 @@
  * @desc 随机生成颜色
  * @return {String} 
  */
-function randomColor() {
+function getRandomColor() {
     return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
 }
 
-module.exports = randomColor;
+module.exports = getRandomColor;
---
+++ getRandomString.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @desc 生成指定长度随机字符串
+ * @param  {Number} length 
+ * @param  {String} mode 
+ * @return {String} 
+ */
+function getRandomString(length, mode) {
+    let resStr = '';
+    let chars = '';
+    if (typeof(length) == 'undefined') {
+        length = 16;
+    }
+    switch (mode) {
+        case 'number':
+            chars = '1234567890';
+            break;
+        default:
+            chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+            break;
+    }
+    let max = chars.length;
+    for (let i = 0; i < max; i++) {
+        resStr += chars[Math.floor(Math.random() * max)];
+    }
+    return resStr;
+}
+
+module.exports = getRandomString;
