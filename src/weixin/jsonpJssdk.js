d490fec8f385699461d31056d1650f24e8e3672f
william <694619951@qq.com>
2018-03-28 11:15:04 +0800

    jsonpJssdk新增appid参数

--- src/weixin/jsonpJssdk.js
+++ src/weixin/jsonpJssdk.js
@@ -1,6 +1,7 @@
 /**
  * 
  * @desc 通过jsonp方式获取微信jssdk的config，并且初始化微信jssdk
+ * @param  {String} appid 公众号appid 
  * @param  {String} url jsonp地址 
  * @param  {Object} shareInfo
  * @param  {Array} apiList  
@@ -9,11 +10,12 @@
 let jsonp = require('../request/jsonp');
 let jssdk = require('./jssdk');
 
-function jsonpJssdk(url, shareInfo, apiList) {
+function jsonpJssdk(appid, url, shareInfo, apiList) {
     jsonp({
         url: url,
         data: {
-            jssdk_url: location.href
+            jssdk_url: location.href,
+            appid: appid
         },
         success: function(config) {
             jssdk(config, shareInfo, apiList);
